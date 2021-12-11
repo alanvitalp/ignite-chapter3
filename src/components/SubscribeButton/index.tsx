9f9e72245f5f1cd74f10c58cf3c3b7eaa47565ec
Alan <alanvital18@gmail.com>
2021-12-11 00:58:41 -0300

    before stripe integration checkpoint

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+export const SubscribeButton = () => {
+  return (
+    <button type="button" className={styles.subscribeButton}>Subscribe</button>
+  )
+}
