ab6779817bae9c68264ce5666514f733ea50fe1e
Alan <alanvital18@gmail.com>
2021-12-17 00:14:19 -0300

    finish chapter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+import { stripe } from '../services/stripe'
 
-export default function Home () {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home ({ product }: HomeProps) {
   return (
     <>
      <Head>
@@ -17,7 +26,7 @@ export default function Home () {
             Get access to all the publitacions <br />
             <span>for $9.90 month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
@@ -26,3 +35,21 @@ export default function Home () {
    
   )
 }
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1K7W8fF2X5ssL1l7gIufasaX')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+  return {
+    props: {
+      product
+    }
+  }
+} 
